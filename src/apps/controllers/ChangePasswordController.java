2139d0c2fefd3c2b2e84c4ef734e2d71eac7d337
ItsBlackGenius <itsblackgenius@gmial.com>
2021-06-18 15:21:27 +0000

    Added functionalities to the 'Cancel' button on the change password screen.

--- src/apps/controllers/ChangePasswordController.java
+++ src/apps/controllers/ChangePasswordController.java
@@ -1,5 +1,22 @@
 package apps.controllers;
 
-public class ChangePasswordController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
+public class ChangePasswordController {
+    @FXML
+    private Button cancel_button;
+    @FXML
+    public void cancel_button_onclick() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../resources/views/login.fxml")));
+        Stage stage = (Stage) cancel_button.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
 }
