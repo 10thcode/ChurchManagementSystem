094eb88edc7b173f23f7e8ed81df7a33aed0e981
ItsBlackGenius <itsblackgenius@gmial.com>
2021-06-21 23:15:49 +0000

    Attendance form added

--- src/apps/Main.java
+++ src/apps/Main.java
@@ -11,8 +11,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../resources/views/login.fxml")));
-        primaryStage.setMinWidth(1200);
-        primaryStage.setMinHeight(700);
         primaryStage.setTitle("Church Management System - Login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
