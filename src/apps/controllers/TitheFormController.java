c8cc5859287daeaed2e09a3021673f8533c96407
ItsBlackGenius <itsblackgenius@gmial.com>
2021-06-27 19:34:47 +0000

    fixed size problems for tithe form and attendance form added visitors registration window on tithe form.

--- src/apps/controllers/TitheFormController.java
+++ src/apps/controllers/TitheFormController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -19,6 +20,14 @@ public class TitheFormController {
         stage.setScene(new Scene(root));
         stage.setResizable(false);
         stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+
+    public void visitors_registration_onclick() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().
+                getResource("../../resources/views/visitors_registration.fxml")));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
         stage.show();
     }
 }
