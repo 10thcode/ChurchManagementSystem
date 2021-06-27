fd44dcaba6bfb13b85e402edeb6cd90b99341797
The-Nemeziz <ayim334@gmail.com>
2021-06-27 19:15:29 +0000

    .

--- src/apps/controllers/AttendanceRegisterController.java
+++ src/apps/controllers/AttendanceRegisterController.java
@@ -1,6 +1,7 @@
 package apps.controllers;
 import animatefx.animation.FadeIn;
 import apps.DbManuplation.DbManipulate;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,11 +11,12 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
 
-public class AttendanceRegisterController {
+public class AttendanceRegisterController implements Serializable {
 
     @FXML
     public TableView Table;
@@ -30,6 +32,9 @@ public class AttendanceRegisterController {
     private TableColumn no_of_children;
     @FXML
     private TableColumn total;
+    String TABLE_NAME = "dbo.ATTENDANCE_TABLE";
+    private ResultSet resultSet;
+
 
     private String Date;
     private String Service;
@@ -37,8 +42,45 @@ public class AttendanceRegisterController {
     private int num_of_female;
     private int num_of_children;
     private int Total;
+    DbManipulate dbManipulate;
+
+    public void setTable(TableView table) {
+        Table = table;
+    }
+
+    public ObservableList data = FXCollections.observableArrayList();
+
+    public void readData(){
+        String Query = "SELECT * FROM " + TABLE_NAME;
+        resultSet = dbManipulate.retrieveData(Query);
+
+        try{
+            if (resultSet != null){
+                while (resultSet.next()){
+                  Date=  resultSet.getString(0);
+                  Service = resultSet.getString(1);
+                  num_of_male = resultSet.getInt(2);
+                  num_of_female = resultSet.getInt(3);
+                  num_of_children = resultSet.getInt(4);
+                  Total = resultSet.getInt(5);
+                  System.out.println("yaay");
+
+
+                data.addAll(Date, Service, num_of_male, num_of_female, num_of_children);
+
+                Table.setItems(data);
+
+
+                }
+            }
+        }catch (SQLException e){
+
+        }
+
+        readData();
 
 
 
+    }
 
 }
--- src/apps/controllers/OfferingFormController.java
+++ src/apps/controllers/OfferingFormController.java
@@ -1,4 +1,20 @@
 package apps.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class OfferingFormController {
+
+    ObservableList<String> service_dropdown_elements = FXCollections.observableArrayList("First Service",
+            "Second Service", "Third Service", "Fourth Service");
+
+    @FXML private ChoiceBox<String> choiceBox;
+
+    public void initialize(){
+        choiceBox.getItems().addAll(service_dropdown_elements);
+    }
+
+
 }
