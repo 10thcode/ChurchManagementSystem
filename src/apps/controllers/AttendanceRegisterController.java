f2865d17f275ddecbc883e4de46f1fa79d9f7990
The-Nemeziz <ayim334@gmail.com>
2021-06-27 18:26:12 +0000

    .

--- src/apps/controllers/AttendanceRegisterController.java
+++ src/apps/controllers/AttendanceRegisterController.java
@@ -1,6 +1,7 @@
 package apps.controllers;
 import animatefx.animation.FadeIn;
 import apps.DbManuplation.DbManipulate;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,10 +16,20 @@ import java.util.Objects;
 
 public class AttendanceRegisterController {
 
-
+    @FXML
     public TableView Table;
     @FXML
     private TableColumn date;
+    @FXML
+    private TableColumn service;
+    @FXML
+    private TableColumn no_of_male;
+    @FXML
+    private TableColumn no_of_female;
+    @FXML
+    private TableColumn no_of_children;
+    @FXML
+    private TableColumn total;
 
     private String Date;
     private String Service;
@@ -26,9 +37,6 @@ public class AttendanceRegisterController {
     private int num_of_female;
     private int num_of_children;
     private int Total;
-
-
-
 
 
 
