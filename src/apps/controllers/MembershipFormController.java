bf49b8bea363b1007958928d07892750659ca471
ItsBlackGenius <itsblackgenius@gmial.com>
2021-07-22 09:31:37 +0000

    Added option to add new element to choiceboxes in membership form.

--- src/apps/controllers/MembershipFormController.java
+++ src/apps/controllers/MembershipFormController.java
@@ -1,21 +1,24 @@
 package apps.controllers;
 
+import animatefx.animation.FadeIn;
 import apps.DbManuplation.DbManipulate;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MembershipFormController {
     public TextField membership_id_field;
@@ -43,25 +46,28 @@ public class MembershipFormController {
     public ChoiceBox<String> cr_occupation_choicebox;
     public TextField search_field;
     public TextField cr_name_field;
+    public TextField add_textfield;
+    public Button add_button;
+    public Button add_cancel_button;
 
+    private ChoiceBox<String> source;
 
     public ResultSet resultSet;
     DbManipulate dbManipulate = new DbManipulate();
 
-
     ObservableList<String> gender_dropdown_elements = FXCollections.observableArrayList("Male", "Female");
     ObservableList<String> marital_status_dropdown_elements = FXCollections.observableArrayList("Single",
-            "Married", "Divorced");
-    ObservableList<String> children_number_dropdown_elements = FXCollections.observableArrayList("1", "2",
-            "3", "4", "5", "6", "7", "8", "9", "10", "None");
+            "Married", "Divorced", "Separated");
+    ObservableList<String> children_number_dropdown_elements = FXCollections.observableArrayList( "None", "1", "2",
+            "3", "4", "5", "6", "7", "8", "9", "10", "Add New");
     ObservableList<String> occupation_dropdown_elements = FXCollections.observableArrayList("Teacher",
-            "Trader", "Lawyer", "Accountant", "Mechanic");
+            "Trader", "Lawyer", "Accountant", "Mechanic", "Add New");
     ObservableList<String> region_dropdown_elements = FXCollections.observableArrayList("Volta", "Eastern",
             "Central", "Ashanti", "Northern", "Greater Accra", "Western", "Upper West", "Upper East", "Brong Ahafo",
             "Ahafo", "North East", "Oti", "Western North", "Bono", "Cape Coast", "Savannah");
-    ObservableList<String> nationality_dropdown_elements = FXCollections.observableArrayList("Ghanaian", "Nigerian",
-            "Togolese");
-    public ObservableList<String> education_level_dropdown_elements = FXCollections.observableArrayList("Primary",
+    ObservableList<String> nationality_dropdown_elements = FXCollections.observableArrayList("Ghanaian",
+            "Nigerian", "Togolese", "Add New");
+    ObservableList<String> education_level_dropdown_elements = FXCollections.observableArrayList("Primary",
             "Junior High", "Senior High", "Tertiary");
     ObservableList<String> relationship_dropdown_elements = FXCollections.observableArrayList("Father", "Mother",
             "Uncle", "Aunt", "Son", "Daughter");
@@ -69,7 +75,7 @@ public class MembershipFormController {
     List<TextField> required_fields = new ArrayList<>();
     List<ChoiceBox<String>> required_dropdowns = new ArrayList<>();
 
-    public  void  initialize(){
+    public  void  initialize() throws IOException {
         marital_status_choicebox.getItems().addAll(marital_status_dropdown_elements);
         children_num_choicebox.getItems().addAll(children_number_dropdown_elements);
         cr_occupation_choicebox.getItems().addAll(occupation_dropdown_elements);
@@ -79,6 +85,43 @@ public class MembershipFormController {
         nationality_choicebox.getItems().addAll(nationality_dropdown_elements);
         region_choicebox.getItems().addAll(region_dropdown_elements);
         gender_choicebox.getItems().addAll(gender_dropdown_elements);
+
+        try {
+            File file = new File("children_num_choicebox.txt");
+            Scanner scn = new Scanner(file);
+            while (scn.hasNextLine()) {
+                String data = scn.nextLine();
+                children_num_choicebox.getItems().add(children_num_choicebox.getItems().size() - 1, data);
+            }
+            scn.close();
+        } catch (FileNotFoundException e) {
+        }
+
+        try {
+            File file = new File("occupation_choicebox.txt");
+            Scanner scn = new Scanner(file);
+            while (scn.hasNextLine()) {
+                String data = scn.nextLine();
+                occupation_choicebox.getItems().add(occupation_choicebox.getItems().size() -1, data);
+            }
+            scn.close();
+        } catch (FileNotFoundException e) {
+        }
+
+        try {
+            File file = new File("nationality_choicebox.txt");
+            Scanner scn = new Scanner(file);
+            while (scn.hasNextLine()) {
+                String data = scn.nextLine();
+                nationality_choicebox.getItems().add(nationality_choicebox.getItems().size() -1, data);
+            }
+            scn.close();
+        } catch (FileNotFoundException e) {
+        }
+
+        add_button.setManaged(false);
+        add_textfield.setManaged(false);
+        add_cancel_button.setManaged(false);
 
         required_fields.add(membership_id_field);
         required_fields.add(dob_field);
@@ -95,7 +138,65 @@ public class MembershipFormController {
         required_dropdowns.add(occupation_choicebox);
         required_dropdowns.add(region_choicebox);
         required_dropdowns.add(nationality_choicebox);
+
+        occupation_choicebox.getSelectionModel().selectedIndexProperty()
+                .addListener((ObservableValue<? extends Number> ov, Number old, Number new_val) ->{
+            if (occupation_choicebox.getSelectionModel().isSelected(occupation_choicebox.getItems().size() -1)){
+                source = occupation_choicebox;
+                FadeIn fadeIn = new FadeIn();
+                fadeIn.setNode(add_textfield);
+                fadeIn.play();
+                FadeIn add_button_fadeIn = new FadeIn();
+                add_button_fadeIn.setNode(add_button);
+                add_button_fadeIn.play();
+                add_textfield.setVisible(true);
+                add_textfield.setManaged(true);
+                add_button.setVisible(true);
+                add_button.setManaged(true);
+                add_cancel_button.setVisible(true);
+                add_cancel_button.setManaged(true);
+            }
+        });
+
+        children_num_choicebox.getSelectionModel().selectedIndexProperty()
+                .addListener((ObservableValue<? extends Number> ov, Number old, Number new_val) ->{
+            if (children_num_choicebox.getSelectionModel().isSelected(children_num_choicebox.getItems().size() -1)){
+                source = children_num_choicebox;
+                FadeIn fadeIn = new FadeIn();
+                fadeIn.setNode(add_textfield);
+                fadeIn.play();
+                FadeIn add_button_fadeIn = new FadeIn();
+                add_button_fadeIn.setNode(add_button);
+                add_button_fadeIn.play();
+                add_textfield.setVisible(true);
+                add_textfield.setManaged(true);
+                add_button.setVisible(true);
+                add_button.setManaged(true);
+                add_cancel_button.setVisible(true);
+                add_cancel_button.setManaged(true);
+            }
+        });
+
+        nationality_choicebox.getSelectionModel().selectedIndexProperty()
+                .addListener((ObservableValue<? extends Number> ov, Number old, Number new_val) ->{
+            if (nationality_choicebox.getSelectionModel().isSelected(nationality_choicebox.getItems().size() -1)){
+                source = nationality_choicebox;
+                FadeIn fadeIn = new FadeIn();
+                fadeIn.setNode(add_textfield);
+                fadeIn.play();
+                FadeIn add_button_fadeIn = new FadeIn();
+                add_button_fadeIn.setNode(add_button);
+                add_button_fadeIn.play();
+                add_textfield.setVisible(true);
+                add_textfield.setManaged(true);
+                add_button.setVisible(true);
+                add_button.setManaged(true);
+                add_cancel_button.setVisible(true);
+                add_cancel_button.setManaged(true);
+            }
+        });
     }
+
     @FXML private void capture_button_onclick() {
     }
 
@@ -195,36 +296,31 @@ public class MembershipFormController {
     }
 
     @FXML private void clear_button_onclick() {
-        setMembershipId(null);
-        setFirstName(null);
-        setLastName(null);
-        setOtherNames(null);
+        setMembershipId("");
+        setFirstName("");
+        setLastName("");
+        setOtherNames("");
         setGender(null);
-        setDob(null);   //To be modified.
-        setContact1(null);
-        setContact2(null);
-        setEmail(null);
-        setAddress(null);
+        setDob("");   //To be modified.
+        setContact1("");
+        setContact2("");
+        setEmail("");
+        setAddress("");
         setMaritalStatus(null);
         setChildrenNum(null);
-        setOccupation(null);
-        setResidence(null);
+        setResidence("");
         setPassportImage("resources/images/default_passport_icon.png");
         setOccupation(null);
-        setHometown(null);
+        setHometown("");
         setRegion(null);
         setNationality(null);
         setEducationLevel(null);
-        setCrName(null);
+        setCrName("");
         setCrRelationship(null);
-        setCrContact(null);
+        setCrContact("");
         setCrOccupation(null);
-        setCrAddress(null);
+        setCrAddress("");
     }
-
-
-
-
 
     @FXML private void save_button_onclick() {
         String Membership_id = membership_id_field.getText();
@@ -268,229 +364,100 @@ public class MembershipFormController {
         }
     }
 
-////    public String getSearch() {
-//        return search_field.getText();
-//    }
-
-//    public String getMembershipId() {
-//        return membership_id_field.getText();
-//    }
-
     public void setMembershipId(String membership_id) {
         this.membership_id_field.setText(membership_id);
     }
-
-//    public String getFirstName() {
-//        return first_name_field.getText();
-//    }
 
     public void setFirstName(String firstName) {
         this.first_name_field.setText(firstName);
     }
 
-//    public String getLastName() {
-//        return last_name_field.getText();
-//    }
-
     public void setLastName(String last_name) {
         this.last_name_field.setText(last_name);
     }
-
-//    public String getOtherNames() {
-//        return other_names_field.getText();
-//    }
 
     public void setOtherNames(String otherNames) {
         this.other_names_field.setText(otherNames);
     }
 
-////    public String getGender() {
-//        return gender_choicebox.getValue();
-//    }
-
     public void setGender(String gender) {
         this.gender_choicebox.setValue(gender);
     }
-
-//    public String getDob() {
-//        return dob_field.getText();
-//    }
 
     public void setDob(String dob) {
         this.dob_field.setText(dob);
     }
 
-//    public String getContact1() {
-//        return contact1_field.getText();
-//    }
-
     public void setContact1(String contact1) {
         this.contact1_field.setText(contact1);
     }
-
-//    public String getContact2() {
-//        return contact2_field.getText();
-//    }
 
     public void setContact2(String contact2) {
         this.contact2_field.setText(contact2);
     }
 
-//    public String getEmail() {
-//        return email.getText();
-//    }
-
     public void setEmail(String email) {
         this.email.setText(email);
     }
-
-//    public String getAddress() {
-//        return address_field.getText();
-//    }
 
     public void setAddress(String address) {
         this.address_field.setText(address);
     }
 
-//    public String getMaritalStatus() {
-//        return marital_status_choicebox.getValue();
-//    }
-
     public void setMaritalStatus(String marital_status) {
         this.marital_status_choicebox.setValue(marital_status);
     }
-
-//    public String getChildrenNum() {
-//        return children_num_choicebox.getValue();
-//    }
 
     public void setChildrenNum(String children_num) {
         this.children_num_choicebox.setValue(children_num);
     }
 
-//    public String getPassportImageview() {
-//        return passport_imageview.getImage().getUrl();
-//    }
-
     public void setPassportImage(String passport_image_url) {
         this.passport_imageview.setImage(new Image(passport_image_url));
     }
-
-//    public String getOccupation() {
-//        return occupation_choicebox.getValue();
-//    }
 
     public void setOccupation(String occupation) {
         this.occupation_choicebox.setValue(occupation);
     }
 
-//    public String getResidence() {
-//        return residence_field.getText();
-//    }
-
     public void setResidence(String residence) {
         this.residence_field.setText(residence);
     }
-
-//    public String getHometown() {
-//        return hometown_field.getText();
-//    }
 
     public void setHometown(String hometown) {
         this.hometown_field.setText(hometown);
     }
 
-//    public String getRegion() {
-//        return region_choicebox.getValue();
-//    }
-
     public void setRegion(String region) {
         this.region_choicebox.setValue(region);
     }
-
-//    public String getNationality() {
-//        return nationality_choicebox.getValue();
-//    }
 
     public void setNationality(String nationality) {
         this.nationality_choicebox.setValue(nationality);
     }
 
-//    public String getEducationLevel() {
-//        return education_level_choicebox.getValue();
-//    }
-
     public void setEducationLevel(String education_level) {
         this.education_level_choicebox.setValue(education_level);
     }
-
-//    public String getCrName() {
-//        return cr_name_field.getText();
-//    }
 
     public void setCrName(String cr_name) {
         this.cr_name_field.setText(cr_name);
     }
 
-//    public String getCrContact() {
-//        return cr_contact_field.getText();
-//    }
-
     public void setCrContact(String cr_contact) {
         this.cr_contact_field.setText(cr_contact);
     }
-
-//    public String getCrAddress() {
-//        return cr_address_field.getText();
-//    }
 
     public void setCrAddress(String cr_address) {
         this.cr_address_field.setText(cr_address);
     }
 
-//    public String getCrRelationship() {
-//        return cr_relationship_choicebox.getValue();
-//    }
-
     public void setCrRelationship(String cr_relationship) {
         this.cr_relationship_choicebox.setValue(cr_relationship);
     }
 
-//    public String getCrOccupation() {
-//        return cr_occupation_choicebox.getValue();
-//    }
-
     public void setCrOccupation(String cr_occupation) {
         this.cr_occupation_choicebox.setValue(cr_occupation);
-    }
-
-    public void contact_field1_onclick(KeyEvent keyEvent) {
-        String text = contact1_field.getText().toLowerCase();
-        char c = text.charAt(text.length()-1);
-        if (c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f'|| c == 'g' || c == 'h' || c == 'i' || c == 'j' || c == 'k' || c == 'l' || c == 'm' || c == 'n' || c == 'o' || c == 'p' || c == 'q' || c == 'r' || c == 's' || c == 't' || c == 'u' || c == 'v' || c == 'w' || c == 'x' || c == 'y' || c == 'z'){
-            setContact1(null);
-        }
-
-    }
-
-    public void contact_field2_onclick(KeyEvent keyEvent) {
-        String text = contact2_field.getText().toLowerCase();
-        char c = text.charAt(text.length()-1);
-        if (c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f'|| c == 'g' || c == 'h' || c == 'i' || c == 'j' || c == 'k' || c == 'l' || c == 'm' || c == 'n' || c == 'o' || c == 'p' || c == 'q' || c == 'r' || c == 's' || c == 't' || c == 'u' || c == 'v' || c == 'w' || c == 'x' || c == 'y' || c == 'z'){
-            setContact2(null);
-        }
-
-
-    }
-
-    public void cr_contact_on_Key_released(KeyEvent keyEvent) {
-
-        String text = cr_contact_field.getText().toLowerCase();
-        char c = text.charAt(text.length()-1);
-        if (c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f'|| c == 'g' || c == 'h' || c == 'i' || c == 'j' || c == 'k' || c == 'l' || c == 'm' || c == 'n' || c == 'o' || c == 'p' || c == 'q' || c == 'r' || c == 's' || c == 't' || c == 'u' || c == 'v' || c == 'w' || c == 'x' || c == 'y' || c == 'z'){
-            setCrContact(null);
-        }
-
     }
 
     public boolean is_valid(){
@@ -513,5 +480,41 @@ public class MembershipFormController {
             }
         }
         return valid_state;
+    }
+
+    public void add_button_onclick() {
+        if (!add_textfield.getText().isBlank()){
+            source.getItems().add(source.getItems().size() - 1,
+                    add_textfield.getText());
+            source.setValue(add_textfield.getText());
+            try(FileWriter fw = new FileWriter(source.getId() + ".txt", true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw))
+            {
+                out.println(add_textfield.getText());
+            } catch (IOException ignored) {
+            }
+            add_button.setManaged(false);
+            add_button.setVisible(false);
+            add_textfield.setText("");
+            add_textfield.setManaged(false);
+            add_textfield.setVisible(false);
+            add_cancel_button.setVisible(false);
+            add_cancel_button.setManaged(false);
+        }else {
+            add_textfield.setStyle("-fx-background-color: #ff8a80");
+        }
+    }
+
+    public void add_cancel_button_onclick() {
+        add_button.setManaged(false);
+        add_button.setVisible(false);
+        add_textfield.setText("");
+        add_textfield.setManaged(false);
+        add_textfield.setVisible(false);
+        add_cancel_button.setVisible(false);
+        add_cancel_button.setManaged(false);
+        add_textfield.setStyle("-fx-background-color: white");
+        source.setValue(null);
     }
 }
